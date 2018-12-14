099623db8fc89596c878ec17bf995d9948a542c5
Yashwant Nagarjuna <ranbonagarjuna@gmail.com>
2018-12-13 23:53:17 -0500

    ..

--- lib/bitcoin_web/controllers/page_controller.ex
+++ lib/bitcoin_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule BitcoinWeb.PageController do
   use BitcoinWeb, :controller
 
   def index(conn, _params) do
-    spawn fn -> Project4.main(100) end
+    spawn fn -> Project4.main(20) end
     render(conn, "index.html")
 
   end
